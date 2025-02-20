7f65cd2fd4d8679416c8e5ee2dddb428f5e333fa
Kamil <kami.krasowski.uk@gmail.com>
2025-02-20 22:07:14 +0000

    node.js server skeleton

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const requestListener = function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
+    switch (req.url) {
+        case '/':
+            res.writeHead(200);
+            res.end('{"message": "Hello World!"}');
+            break
+        case '/login':
+            res.writeHead(200);
+            res.end('{"message": "Login Page"}');
+            break
+    }
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, hostname, () => {
+    console.log(`Server is running on https://${hostname}:${port}`);
+});
